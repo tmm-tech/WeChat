0e8f711754a929a5058aeacb2de3994b5684774f
Tony Mwangi Mugi <tonymugi074@gmail.com>
2023-06-26 13:10:51 +0300

    Third Commit

---
+++ wechat_BackEnd/Authentication/index.js
@@ -0,0 +1,20 @@
+// server.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3030;
+
+app.use(express.json());
+app.use(cors());
+
+
+// Routes
+app.use('/users', UserRoutes);
+// Define your routes here
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ wechat_BackEnd/Authentication/routes/UserRoutes.js
@@ -0,0 +1,19 @@
+const UserRoutes = require('express').Router();
+const {
+    createUser,
+    getAUser,
+    loginUser,
+    Logout,
+} = require('../controllers/UserControllers');
+
+// create and insert data into the table
+UserRoutes.post('/register', createUser)
+    //read for a specific id
+UserRoutes.get('/user/:id', getAUser)
+    // login a user
+UserRoutes.post('/login', loginUser)
+    // logout user
+UserRoutes.post('/logout/:email', Logout)
+
+
+module.exports = UserRoutes
